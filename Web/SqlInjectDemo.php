6e2dac0da417c4aa16e4981df397c42b5d0a2351
Tag Bao <7ym0n@users.noreply.github.com>
2016-03-08 17:07:14 +0800

    Update SqlInjectDemo.php

--- Web/SqlInjectDemo.php
+++ Web/SqlInjectDemo.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "meiyoumima", "mywebretine");
+$mysqli = new mysqli("localhost", "root", "meiyoumima", "mySqlInject");
 
 /* check connection */
 if ($mysqli->connect_errno) {
